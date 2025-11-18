950d9611741b8b7504582943d00432d13168264a
anna <abfoldberg@gmail.com>
2025-11-18 12:37:08 +0100

    add exchanges

--- RabbitMQ/Exchanges.cs
+++ RabbitMQ/Exchanges.cs
@@ -2,6 +2,8 @@ namespace Messaging.RabbitMQ;
 
 public static class Exchanges
 {
+    public const string ImageUploaded = "image.uploaded";
+    public const string RecognitionCompleted = "recognition.completed";
     public const string AnalysisCommands = "analysis.commands";
     public const string AnalysisStarted = "analysis.started";
     public const string AnalysisCompleted = "analysis.completed";
